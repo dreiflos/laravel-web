4dce5bb77cf1fbe2f81868a7861a807d2d693cb3
dreiflos <andreifloresrodrigues@gmail.com>
2026-02-25 22:21:23 -0400

    Initials

--- tads5/app/Http/Controllers/AuthController.php
+++ tads5/app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ class AuthController extends Controller
     public function login(Request $request){
     $request->validate([
         'email' => 'required|string|email|max:200',
-        'password' => 'required|string',
+        'password' => 'required|string|min:8',
         'device_name' => 'required|string'
     ]);
 
